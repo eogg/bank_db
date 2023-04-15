5fcd5021eedcc2204c5744bef82c66690e345cd7
eogg <elian.garciagonzalez@stu.pfisd.net>
2023-04-15 12:04:32 -0500

    f

--- database_functions.py
+++ database_functions.py
@@ -13,5 +13,5 @@ def deposit(accountID, accountPassword, depo):
     cursor.execute(f'UPDATE bank_database.user SET balance = balance + {depo} WHERE accountid = {accountID} AND pin_code = {accountPassword}')
     cursor.execute(f'SELECT balance FROM bank_database.user WHERE accountid = {accountID} AND pin_code = {accountPassword}')
     for item in cursor:
-        print(f"Successfully deposited ${depo} to account number {accountID}. The new balance for account number {accountID} is " + item[0])
+        print(f'Successfully deposited ${depo} to account number {accountID}. The new balance for account number {accountID} is {item[0]}.')
     

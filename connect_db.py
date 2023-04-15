50cd8d0ee0f20487478dbaf1a79677e513abebd0
eogg <elian.garciagonzalez@stu.pfisd.net>
2023-04-15 12:35:13 -0500

    back

--- connect_db.py
+++ connect_db.py
@@ -15,7 +15,7 @@ for item in cursor:
 
 database_functions.getBalance()
 database_functions.deposit(8999)
-
+database_functions.widthdraw(8999)
 # By End of Week - Students have created SQL tables needed for the app and have successfully created all (or most) 
 # functions for all tasks (check balance, deposit, withdraw, create account, delete account, modify account) 
 # and create tables for account data.

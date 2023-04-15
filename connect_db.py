3e333d93838af6319bf0b5e5d69e9cb70e4e099d
eogg <elian.garciagonzalez@stu.pfisd.net>
2023-04-15 11:50:24 -0500

    Fixing

--- connect_db.py
+++ connect_db.py
@@ -3,16 +3,16 @@ import database_functions
 
 connection = mysql.connector.connect(user = 'root', database = 'bank_database',password = '113377')
 cursor = connection.cursor()
-# testQuery = ("SELECT * FROM bank_database.user")
-# cursor.execute(testQuery)
-# for item in cursor:
-#     for thing in item:
-#         print(thing, end = ", ")
-#     print()
+testQuery = ("SELECT * FROM bank_database.user")
+cursor.execute(testQuery)
+for item in cursor:
+    for thing in item:
+        print(thing, end = ", ")
+    print()
 
 
 acID = int(input("Enter your Account ID: "))
 acPw = int(input("Enter your Pin Code: "))
 
 
-getBalance(acID, acPW)
+database_functions.getBalance(acID, acPw)
--- database_functions.py
+++ database_functions.py
@@ -1,5 +1,7 @@
 import mysql.connector
 
+connection = mysql.connector.connect(user = 'root', database = 'bank_database',password = '113377')
+cursor = connection.cursor()
 def getBalance(accountID, accountPassword):
     cursor.execute(f'SELECT balance FROM bank_database.user WHERE accountid = {accountID} AND pin_code = {accountPassword}')
     for item in cursor:

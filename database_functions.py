0780225308ad2b03318d64ffd39408a62945cfd5
eogg <elian.garciagonzalez@stu.pfisd.net>
2023-04-15 12:34:25 -0500

    pls

--- database_functions.py
+++ database_functions.py
@@ -18,6 +18,15 @@ def deposit(depo):
     cursor.execute(f'SELECT balance FROM bank_database.user WHERE accountid = {acID} AND pin_code = {acPW}')
     for item in cursor:
         print(f'Successfully deposited ${depo} to account number {acID}. The new balance for account number {acID} is {item[0]}.')
+
+def widthdraw(amt):
+    acID = int(input("Enter your Account ID: "))
+    acPW = int(input("Enter your Pin Code: "))
+    depo = int(input("How much would you like to deposit into your account?"))
+    cursor.execute(f'UPDATE bank_database.user SET balance = balance - {amt} WHERE accountid = {acID} AND pin_code = {acPW}')
+    cursor.execute(f'SELECT balance FROM bank_database.user WHERE accountid = {acID} AND pin_code = {acPW}')
+    for item in cursor:
+        print(f'Successfully widthdrew ${amt} to account number {acID}. The new balance for account number {acID} is {item[0]}.')
     
     
 # def update_account(accountID, accountPassword):

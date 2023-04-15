f29d231bccb129a81c7449cc18a679d4c1948063
eogg <elian.garciagonzalez@stu.pfisd.net>
2023-04-15 12:37:34 -0500

    Working now

--- database_functions.py
+++ database_functions.py
@@ -10,19 +10,19 @@ def getBalance():
     for item in cursor:
         print(item[0])
 
-def deposit(depo):
+def deposit():
     acID = int(input("Enter your Account ID: "))
     acPW = int(input("Enter your Pin Code: "))
-    depo = int(input("How much would you like to deposit into your account?"))
+    depo = int(input("How much would you like to deposit into your account? "))
     cursor.execute(f'UPDATE bank_database.user SET balance = balance + {depo} WHERE accountid = {acID} AND pin_code = {acPW}')
     cursor.execute(f'SELECT balance FROM bank_database.user WHERE accountid = {acID} AND pin_code = {acPW}')
     for item in cursor:
         print(f'Successfully deposited ${depo} to account number {acID}. The new balance for account number {acID} is {item[0]}.')
 
-def widthdraw(amt):
+def widthdraw():
     acID = int(input("Enter your Account ID: "))
     acPW = int(input("Enter your Pin Code: "))
-    depo = int(input("How much would you like to deposit into your account?"))
+    amt = int(input("How much would you like to withdraw from your account? "))
     cursor.execute(f'UPDATE bank_database.user SET balance = balance - {amt} WHERE accountid = {acID} AND pin_code = {acPW}')
     cursor.execute(f'SELECT balance FROM bank_database.user WHERE accountid = {acID} AND pin_code = {acPW}')
     for item in cursor:

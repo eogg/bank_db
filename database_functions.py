6067b6492c7116b347c2ad957d83276226e5a7d9
eogg <elian.garciagonzalez@stu.pfisd.net>
2023-04-17 23:36:38 -0500

    Working on UI

--- database_functions.py
+++ database_functions.py
@@ -29,18 +29,15 @@ def widthdraw():
         print(f'Successfully widthdrew ${amt} to account number {acID}. The new balance for account number {acID} is {item[0]}.')
     
 def create_account():
-    acID = int(input("Enter your Account ID: "))
-    acPW = int(input("Enter your Pin Code: "))
+    username = input("What name would you like the account to be under? ")
     amt = int(input("How much would you like to initially deposit into your account? "))
     indob = input("Please enter your date of birth in the format of mm/dd/yyyy. ")
     while True:
-        pin = int(input("Please enter the pin you'd like to use for your account"))
-        pin2 = int(input("Please enter the pin again."))
+        pin = int(input("Please enter the pin you'd like to use for your account: "))
+        pin2 = int(input("Please enter the pin again. "))
         if(pin == pin2):
             break
         else:
             print("Please try again.")
             continue
-    cursor.execute(f'INSERT INTO bank_database.user (accountid, accountname, balance, dob, pin_code) VALUES ({acID}, {acPW}, {amt}, {indob}, {pin})')
-        
-# def update_account(accountID, accountPassword):
+    cursor.execute(f'INSERT INTO bank_database.user (accountname, balance, dob, pin_code) VALUES ({username}, {amt}, {indob}, {pin})')
